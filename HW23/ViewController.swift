485d027a7b91f6d633e778cf4d1e06eaf05802b3
Datkayim <dkeldibekova08@gmail.com>
2023-02-21 14:20:24 +0600

    Initial Commit

---
+++ HW23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW23
+//
+//  Created by Jarae on 21/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
